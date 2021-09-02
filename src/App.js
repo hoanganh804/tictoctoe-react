4f0dfa5954fd3caa98be0769e55a13dd7897a2e1
anhlhde140207 <lamhoanganh2000@gmail.com>
2021-09-03 00:20:31 +0700

    up

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import "./App.css";
 import TictoctoeApp from "./components/TictoctoeApp";
 import "antd/dist/antd.css";
 import Login from "./components/Login";
-import { BrowserRouter as Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/tictoctoe-react/" component={Login} />
-        <Route
-          exact
-          path="/tictoctoe-react/tictoctoeapp"
-          component={TictoctoeApp}
-        />
-      </Switch>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/tictoctoe-react/" component={Login} />
+          <Route
+            exact
+            path="/tictoctoe-react/tictoctoeapp"
+            component={TictoctoeApp}
+          />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
